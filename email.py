311594ed9eb3ba8c65f51c36694195a74d7c05b5
Mouad Kommir <mouadkommir@gmail.com>
2022-12-27 10:01:58 +0100

    Update activation email .html file

--- email.py
+++ email.py
@@ -15,11 +15,11 @@ class ActivationEmail(BaseEmailMessage):
         context = super().get_context_data()
 
         user = context.get("user")
-        context["logo_file_path"] = lava_settings.LOGO_FILE_PATH
-        context["email"] = user.email
         context["uid"] = utils.encode_uid(user.pk)
         context["token"] = default_token_generator.make_token(user)
         context["url"] = settings.ACTIVATION_URL.format(**context)
+        context["logo_file_path"] = lava_settings.LOGO_FILE_PATH
+        context["email"] = user.email
         return context
 
 
