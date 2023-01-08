d7c51f74d62077b36c3a61fe79522b58bad426cf
Mouad Kommir <mouadkommir@gmail.com>
2023-01-08 11:40:50 +0100

    Add generate_key management command

---
+++ management/commands/generate_key.py
@@ -0,0 +1,40 @@
+import os
+import logging
+
+from django.core.management.utils import get_random_secret_key
+from django.core.management.base import BaseCommand
+from django.conf import settings
+
+
+class Command(BaseCommand):
+    help = """
+        This command generates a 128 long secret key and saves it to BASE_DIR/secret.key
+        To change the key name, call the command using the parameter -name
+        ex:
+            python manage.py generate_key -name project_name
+    """
+
+    def add_arguments(self, parser):
+        parser.add_argument(
+            '-name',
+            nargs='?',
+            default='secret',
+            type=str
+        )
+
+    def handle(self, *args, **options):
+
+        name = options["name"] or 'secret'
+        path = os.path.join(settings.BASE_DIR, f"{name}.key")
+        secret = (
+            f'{get_random_secret_key()}'
+            f'{get_random_secret_key()}'
+            f'{get_random_secret_key()}'
+        )[:128]
+        with open(path, 'w') as f:
+            f.write(secret)
+        
+        logging.info(
+            f"A key has been generated at {path}\n"
+            f"Please copy it to /etc/secrets/[project_name]_secret_key.txt"
+        )
