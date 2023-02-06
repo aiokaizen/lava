820479b7207456f3185c15b51da35901107e1e97
Mouad Kommir <k.mouad@ekblocks.com>
2023-02-06 17:31:00 +0100

    Add some messages and comments to exception handling

--- exceptions.py
+++ exceptions.py
@@ -1,12 +1,19 @@
 from django.utils.translation import gettext_lazy as _
+from django.conf import settings
+from django.http import Http404
 
 from rest_framework.views import exception_handler
-from rest_framework.exceptions import NotAuthenticated, PermissionDenied
+from rest_framework.exceptions import NotAuthenticated, PermissionDenied, NotFound
 from rest_framework.response import Response
 from rest_framework import status
 
-from lava.messages import FORBIDDEN_MESSAGE, NOT_AUTHENTICATED_MESSAGE
-from lava.error_codes import PERMISSION_DENIED_ERROR_CODE, NOT_AUTHENTICATED_ERROR_CODE
+from lava.messages import (
+    FORBIDDEN_MESSAGE, NOT_AUTHENTICATED_MESSAGE, UNKNOWN_ERROR_MESSAGE, NOT_FOUND_MESSAGE
+)
+from lava.error_codes import (
+    PERMISSION_DENIED_ERROR_CODE, NOT_AUTHENTICATED_ERROR_CODE, UNKNOWN,
+    NOT_FOUND_ERROR_CODE
+)
 
 
 class LavaBaseException(Exception):
@@ -19,7 +26,7 @@ class LavaBaseException(Exception):
 
 def lava_drf_exception_handler(exc, context):
     from lava.utils import Result
-    
+
     if isinstance(exc, NotAuthenticated):
         return Response(
             Result(False, NOT_AUTHENTICATED_MESSAGE, error_code=NOT_AUTHENTICATED_ERROR_CODE).to_dict(),
@@ -30,5 +37,11 @@ def lava_drf_exception_handler(exc, context):
             Result(False, FORBIDDEN_MESSAGE, error_code=PERMISSION_DENIED_ERROR_CODE).to_dict(),
             status=status.HTTP_403_FORBIDDEN
         )
+
+    # if settings.DEBUG == False and isinstance(exc, Exception):
+    #     return Response(
+    #         Result(False, UNKNOWN_ERROR_MESSAGE, error_code=UNKNOWN).to_dict(),
+    #         status=status.HTTP_500_INTERNAL_SERVER_ERROR
+    #     )
 
     return exception_handler(exc, context)
--- messages.py
+++ messages.py
@@ -20,3 +20,5 @@ HTTP_403_MESSAGE = {"detail": FORBIDDEN_MESSAGE}
 NOT_AUTHENTICATED_MESSAGE =  _('Authentication credentials were not provided.')
 
 INVALID_FORMAT_MESSAGE = _("Invalid format.")
+
+NOT_FOUND_MESSAGE = _('Not found.')
