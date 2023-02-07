e1c7f6bc88b74a2cf87f2882c4f2c4c09b8ed5df
Mouad Kommir <mouadkommir@gmail.com>
2023-02-07 19:02:40 +0100

    Change trash manager on group model

--- models/models.py
+++ models/models.py
@@ -37,7 +37,9 @@ from lava.utils import (
     generate_password,
     strtobool,
 )
-from lava.managers import LavaUserManager, TrashBaseModelManager
+from lava.managers import (
+    DefaultBaseModelManager, LavaUserManager
+)
 
 try:
     from firebase_admin import messaging
@@ -233,7 +235,7 @@ class Group(BaseModelMixin, BaseGroupModel):
     #     'self', verbose_name=_("Parent"), on_delete=models.PROTECT,
     #     related_name="sub_groups", null=True, blank=True
     # )
-    trash = TrashBaseModelManager()
+    trash = DefaultBaseModelManager()
     
     def create(self, user=None, m2m_fields=None):
         if user and not can_add_group(user):
