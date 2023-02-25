756e7ea8ff2cf63ba39b59f5f85324511169a08b
Mouad Kommir <mouadkommir@gmail.com>
2023-02-25 19:50:37 +0100

    + Add ExactLengthValidator and qr_code_validator + Add AssertNumThreads to tests.init file

---
+++ tests/__init__.py
@@ -0,0 +1,19 @@
+import threading
+
+__all__ = [
+    "AssertNumThreads"
+]
+
+
+class AssertNumThreads:
+    message = "Actual number of threads ({}) is different than the expected one ({})."
+    def __init__(self, expected_threads):
+        self.expected_threads = expected_threads
+
+    def __enter__(self):
+        self.initial_num_threads = threading.active_count()
+
+    def __exit__(self, exc_type, exc_val, exc_tb):
+        actual_num_threads = threading.active_count() - self.initial_num_threads
+        assert actual_num_threads == self.expected_threads, \
+            self.message.format(actual_num_threads, self.expected_threads)
--- validators.py
+++ validators.py
@@ -1,6 +1,11 @@
-from django.core.validators import EmailValidator, URLValidator, RegexValidator
+import qrcode
+
+from django.core.validators import (
+    EmailValidator, URLValidator, RegexValidator, BaseValidator
+)
 from django.core.exceptions import ValidationError
-from django.utils.translation import gettext_lazy as _
+from django.utils.deconstruct import deconstructible
+from django.utils.translation import gettext_lazy as _, ngettext_lazy
 from django.conf import settings
 
 from lava import settings as lava_settings
@@ -47,6 +52,30 @@ class SchemelessURLValidator(URLValidator):
                 )
             value = "https://www.ekblocks.com" + value
         super().__call__(value)
+
+
+@deconstructible
+class ExactLengthValidator(BaseValidator):
+    message = ngettext_lazy(
+        'Ensure this value has exactly %(limit_value)d character (it has %(show_value)d).',
+        'Ensure this value has exactly %(limit_value)d characters (it has %(show_value)d).',
+        'limit_value')
+    code = 'exact_length'
+
+    def compare(self, a, b):
+        return a != b
+
+    def clean(self, x):
+        return len(x)
+
+
+def qr_code_validator(value):
+    try:
+        qr = qrcode.QRCode(version=None, error_correction=qrcode.constants.ERROR_CORRECT_L)
+        qr.add_data(value)
+        qr.make(fit=True)
+    except:
+        raise ValidationError(_('Invalid QR code'))
 
 
 def validate_notifications_settings(value):
