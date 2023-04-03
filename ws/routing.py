8fe0f30380c6ba4510b9701244a013eed08d7d8b
Mouad Kommir <mouadkommir@gmail.com>
2023-04-03 22:58:52 +0000

    Update routing.py to set the DJANGO_SETTINGS_MODULE before importing models

--- ws/routing.py
+++ ws/routing.py
@@ -3,6 +3,8 @@ import os
 from django.core.asgi import get_asgi_application
 from django.urls import path
 
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'librarian.settings.settings_asgi')
+
 from channels.routing import ProtocolTypeRouter, URLRouter
 
 from lava.ws.consumers import ChatConsumer, NotificationConsumer
@@ -14,8 +16,6 @@ websocket_urlpatterns = [
     path('ws/chat/<int:conversation_id>/', ChatConsumer.as_asgi()),
     path('ws/notifications/', NotificationConsumer.as_asgi()),
 ]
-
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'librarian.settings.settings_asgi')
 
 application = ProtocolTypeRouter({
     "http": get_asgi_application(),
