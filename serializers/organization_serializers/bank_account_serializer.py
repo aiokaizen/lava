3503afbd9589c5cf2fc35c4112aad033530a54a9
Mohamed Ouhammi <o.mohammed@ekblocks.com>
2023-06-05 17:58:44 +0100

    +  add serializer bank_acount in serializers

---
+++ serializers/organization_serializers/bank_account_serializer.py
@@ -0,0 +1,29 @@
+from django.utils.translation import gettext_lazy as _
+
+from rest_framework import  serializers
+from rest_framework.fields import empty
+
+from lava.models.utility_models import Address
+from lava.serializers import ReadOnlyBaseModelSerializer, BaseModelSerializer, UserExerptSerializer
+from lava.models import BankAccount
+
+
+
+# Address serializer
+class BankAccountSerializer(BaseModelSerializer):
+
+    class Meta:
+        model = BankAccount
+        fields = [
+            'id',
+            'name',
+            'balance',
+            'account_holder',
+            'iban',
+            'rib',
+            'bank',
+        ]
+        read_only_fields = [
+            'id'
+        ]
+
