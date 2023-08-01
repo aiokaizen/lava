cddbfe2070ff5fa0c8764d8286ffafdabbd22809
Mouad Kommir <mouadkommir@gmail.com>
2023-08-01 18:52:23 +0100

    Change channels backend

--- project_settings_asgi.py
+++ project_settings_asgi.py
@@ -8,6 +8,9 @@ INSTALLED_APPS = [
 
 CHANNEL_LAYERS = {
     'default': {
-        'BACKEND': "channels.layers.InMemoryChannelLayer"
+        'BACKEND': "channels_redis.core.RedisChannelLayer",
+        'CONFIG': {
+            'hosts': [('127.0.0.1', 9016)],
+        },
     },
 }
